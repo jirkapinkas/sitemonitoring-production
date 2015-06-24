90f195aceefc796854cd96ba116e2de0c525be7f
Jiri Pinkas <jirka.pinkas@gmail.com>
2015-06-24 13:53:18 +0200

    more compact check date + more nicer icons

--- src/main/java/net/sf/sitemonitoring/push/CheckResultDto.java
+++ src/main/java/net/sf/sitemonitoring/push/CheckResultDto.java
@@ -1,5 +1,6 @@
 package net.sf.sitemonitoring.push;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import lombok.Data;
@@ -16,5 +17,28 @@ public class CheckResultDto {
 	private Date finishDate;
 
 	private long responseTime;
+
+	public String getDateInterval() {
+		if (startDate == null && finishDate == null) {
+			return "";
+		}
+		StringBuilder interval = new StringBuilder();
+		if (startDate != null) {
+			interval.append(new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(startDate));
+			interval.append(" - ");
+		}
+		if (finishDate != null) {
+			if (startDate != null) {
+				String startDateDay = new SimpleDateFormat("dd.MM.yyyy").format(startDate);
+				String finishDateDay = new SimpleDateFormat("dd.MM.yyyy").format(finishDate);
+				if(startDateDay.equals(finishDateDay)) {
+					interval.append(new SimpleDateFormat("HH:mm:ss").format(finishDate));
+				} else {
+					interval.append(new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(finishDate));
+				}
+			}
+		}
+		return interval.toString();
+	}
 
 }
---
+++ src/test/java/net/sf/sitemonitoring/push/CheckResultDtoTest.java
@@ -0,0 +1,54 @@
+package net.sf.sitemonitoring.push;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CheckResultDtoTest {
+	
+	CheckResultDto checkResultDto;
+
+	@Before
+	public void setUp() throws Exception {
+		checkResultDto = new CheckResultDto();
+	}
+
+	@Test
+	public void testGetDateInterval1A() throws ParseException {
+		checkResultDto.setStartDate(new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").parse("01.01.2015 03:00:00"));
+		checkResultDto.setFinishDate(new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").parse("01.01.2015 03:00:10"));
+		assertEquals("01.01.2015 03:00:00 - 03:00:10", checkResultDto.getDateInterval());
+	}
+
+	@Test
+	public void testGetDateInterval1B() throws ParseException {
+		checkResultDto.setStartDate(new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").parse("01.01.2015 03:00:00"));
+		checkResultDto.setFinishDate(new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").parse("01.02.2015 03:00:00"));
+		assertEquals("01.01.2015 03:00:00 - 01.02.2015 03:00:00", checkResultDto.getDateInterval());
+	}
+
+	@Test
+	public void testGetDateInterval2() throws ParseException {
+		checkResultDto.setStartDate(null);
+		checkResultDto.setFinishDate(new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").parse("01.01.2015 03:00:10"));
+		assertEquals("", checkResultDto.getDateInterval());
+	}
+
+	@Test
+	public void testGetDateInterval3() throws ParseException {
+		checkResultDto.setStartDate(new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").parse("01.01.2015 03:00:00"));
+		checkResultDto.setFinishDate(null);
+		assertEquals("01.01.2015 03:00:00 - ", checkResultDto.getDateInterval());
+	}
+
+	@Test
+	public void testGetDateInterval4() throws ParseException {
+		assertEquals("", checkResultDto.getDateInterval());
+	}
+
+}
