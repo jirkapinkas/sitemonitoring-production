982e313907f0abbd0abdb78f2f210c4f6ed9829d
Jiri Pinkas <jirka.pinkas@gmail.com>
2015-07-17 12:35:07 +0200

    version 1.13 + nicer icon for localhost email

--- src/main/java/net/sf/sitemonitoring/service/UpgradeService.java
+++ src/main/java/net/sf/sitemonitoring/service/UpgradeService.java
@@ -101,6 +101,11 @@ public class UpgradeService {
 			configurationService.saveExcludingPassword(configuration);
 			configuration = configurationService.find();
 		}
+		if (configuration.getMonitoringVersion().equals("2.1.12")) {
+			configuration.setMonitoringVersion("2.1.13");
+			configurationService.saveExcludingPassword(configuration);
+			configuration = configurationService.find();
+		}
 	}
 
 	private void update(String sql) {
